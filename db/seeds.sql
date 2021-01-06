c6b7b873fa0d24cc90b7e770c0fa1e8f195282da
Max Lechner <mkonlech@gmail.com>
2021-01-05 17:53:37 -0800

    include schema and seed for the MySQL DB

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+-- `burgers` table with at least three entries.
+-- Insert a set of records.
+INSERT INTO burgers (burger_name, devoured) VALUES ('Bacon cheeseburger', TRUE);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Veggie burger', TRUE);
+INSERT INTO burgers (burger_name, devoured) VALUES ('California burger', FALSE);
