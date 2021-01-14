2812fdb4fea15a1313739681ce580c1014a65bc0
Max Lechner <mkonlech@gmail.com>
2021-01-13 18:19:10 -0800

    update user interface

--- config/orm.js
+++ config/orm.js
@@ -88,6 +88,7 @@ function objToSql(ob) {
         cb(result);
       });
     },
+    
     deleteOne: function(table, condition, cb) {
       var queryString = "DELETE FROM " + table;
       queryString += " WHERE ";
--- models/burger.js
+++ models/burger.js
@@ -17,6 +17,7 @@ var burger = {
       cb(res);
     });
   },
+  
   deleteOne: function(condition, cb) {
     orm.deleteOne("burgers", condition, function(res) {
       cb(res);
